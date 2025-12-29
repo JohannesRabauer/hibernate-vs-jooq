d176956d7044185f6b63a416f95cc7b7561034de
Johannes Rabauer <8188460+JohannesRabauer@users.noreply.github.com>
2025-12-29 18:07:04 +0100

    Add PanacheRepository

--- backend-hibernate/src/main/java/backend_jooq/repositories/AddressRepository.java
+++ backend-hibernate/src/main/java/backend_jooq/repositories/AddressRepository.java
@@ -2,29 +2,21 @@ package backend_jooq.repositories;
 
 import backend_jooq.db.dtos.DbAddress;
 import backend_jooq.model.Address;
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
 import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.persistence.EntityManager;
 
 import java.util.List;
 
 
 @ApplicationScoped
-public class AddressRepository {
+public class AddressRepository implements PanacheRepository<Address> {
 
-    private final EntityManager em;
-
-    public AddressRepository(EntityManager em) {
-        this.em = em;
-    }
 
     public List<DbAddress> findAllByCustomerId(int customerId) {
-        var cb = em.getCriteriaBuilder();
-        var cq = cb.createQuery(Address.class);
-        var root = cq.from(Address.class);
-        cq.select(root).where(cb.equal(root.get("customer").get("customerId"), customerId));
-        List<Address> results = em.createQuery(cq).getResultList();
-        return results.stream()
-                .map(a -> new DbAddress(a.getStreet(), a.getCity(), a.getCountry()))
-                .toList();
+        return list("customer.id", customerId)
+                .stream()
+                .map(address ->
+                        new DbAddress(address.getStreet(), address.getCity(), address.getCountry())
+                ).toList();
     }
 }
